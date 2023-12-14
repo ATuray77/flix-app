9dd5efee65c8fd4e06b7d94761a3debddec12665
Aruna Turay <tura_arun@yahoo.com>
2023-12-13 19:35:55 -0500

    working on movie details page

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const global = {
     currentPage: window.location.pathname,
 };
 
-//Fetch data from TMDB API
+//Fetch data from TMDB API. Function displaying popular movies
 async function displayPopularMovies(){
     const { results }= await fetchAPIData('movie/popular');
     
@@ -36,6 +36,7 @@ async function displayPopularMovies(){
     });
 }
 
+//Fetch data from TMDB API. Function displaying popular shows
 async function displayPopularShows(){
     const { results }= await fetchAPIData('tv/popular');
     
@@ -69,6 +70,63 @@ async function displayPopularShows(){
     });
 }
 
+//Display movie details
+async function displayMovieDetails(){
+    const movieId = window.location.search.split('=')[1]; //isolate the id using 'split' method
+    
+    const movie = await fetchAPIData(`movie/${movieId}`); //fetch the data(movie id)
+
+    const div = document.createElement('div'); //create a div on which to append the data
+    //giving the newly created div a text content
+    div.innerHTML = `
+    <div class="details-top">
+          <div>
+          ${
+            movie.poster_path
+                ? ` <img
+                src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+                class="card-img-top"
+                alt="Show Name"
+              />`: ` <img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+         }
+          </div>
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${movie.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
+            <p>
+              ${movie.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              <li>Genre 1</li>
+              <li>Genre 2</li>
+              <li>Genre 3</li>
+            </ul>
+            <a href="#" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span> $1,000,000</li>
+            <li><span class="text-secondary">Revenue:</span> $2,000,000</li>
+            <li><span class="text-secondary">Runtime:</span> 90 minutes</li>
+            <li><span class="text-secondary">Status:</span> Released</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">Company 1, Company 2, Company 3</div>
+        </div>
+    `;
+    document.querySelector('#movie-details').appendChild(div);
+}
 //Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
     //Only use this key for development. Store your key and make requests from a server
@@ -117,7 +175,7 @@ function init() {
         break;
 
         case '/movie-details.html':
-        console.log('Movie-Details');
+        displayMovieDetails();
         break;
 
         case '/tv-details.html':
